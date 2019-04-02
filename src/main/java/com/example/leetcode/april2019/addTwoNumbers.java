9596907c9111d997850c4318e3130d1842c5a92c
GC <1007879951@qq.com>
2019-04-02 18:19:14 +0800

    leetcode 刷题记录

---
+++ src/main/java/com/example/leetcode/april2019/addTwoNumbers.java
@@ -0,0 +1,4 @@
+package com.example.leetcode.april2019;
+
+public class addTwoNumbers {
+}
---
+++ src/main/java/com/example/leetcode/april2019/addTwoNumbersTest/ListNode.java
@@ -0,0 +1,14 @@
+package com.example.leetcode.april2019.addTwoNumbersTest;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+//    public void setNext(ListNode next) {
+//        this.next = next;
+//    }
+
+    public ListNode(int x){
+        val=x;
+    }
+}
