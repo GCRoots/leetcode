ab534ef107de95326ea6a884d4df684668875866
GC <1007879951@qq.com>
2020-01-29 21:28:45 +0800

    BinarySearchTreeDemo

---
+++ src/main/java/BinarySearchTreeDemo/Solution.java
@@ -0,0 +1,13 @@
+package BinarySearchTreeDemo;
+
+/**
+ * @author shipengfei
+ * @data 2020/1/28
+ */
+public class Solution {
+    public static void main(String[] args) {
+        
+    }
+
+
+}
---
+++ src/main/java/BinarySearchTreeDemo/TreeNode.java
@@ -0,0 +1,14 @@
+package BinarySearchTreeDemo;
+
+/**
+ * @author shipengfei
+ * @data 2020/1/28
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) { val = x; }
+
+}
