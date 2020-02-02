2f883a404e0b8590943d64d8b9cda0c6fa73c6f9
GC <1007879951@qq.com>
2020-02-02 23:39:37 +0800

    easy 26 27

--- src/main/java/easy/Solution.java
+++ src/main/java/easy/Solution.java
@@ -1,5 +1,6 @@
 package easy;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
@@ -12,9 +13,9 @@ public class Solution {
     public static void main(String[] args) {
         Solution solution=new Solution();
 
-        int[] nums ={2, 7, 11, 15};
+        int[] nums ={2,2,2,7,7,11,12,14,14,15};
         int target = 9;
-        System.out.println(solution.twoSum(nums,target)[1]);
+        System.out.println(Arrays.toString(solution.twoSum(nums, target)));
 
         System.out.println(-120+"\t"+solution.reverse(-120));
 
@@ -39,6 +40,17 @@ public class Solution {
             System.out.print(l.val+" -> ");
             l=l.next;
         }
+        System.out.println();
+
+        int[] num={0,0,1,1,1,2,2,3,3,4};
+        System.out.println(Arrays.toString(num));
+        System.out.println(solution.removeDuplicates(num));
+        System.out.println(Arrays.toString(num));
+
+        int[] num1={0,0,1,1,1,2,2,3,3,4};
+        System.out.println(Arrays.toString(num1));
+        System.out.println(solution.removeElement(num1,2));
+        System.out.println(Arrays.toString(num1));
 
     }
 
@@ -454,8 +466,73 @@ public class Solution {
      * @param nums
      * @return
      */
+    //嵌套循环法 慢，效率低
     public int removeDuplicates(int[] nums) {
-
-        return 0;
+        int num=nums.length;
+        for (int i=0;i<num-1;i++){
+            while (nums[i]==nums[i+1]&&i<num-1){
+                for (int j=i+1;j<num-1;j++){
+                    nums[j]=nums[j+1];
+                }
+                num--;
+            }
+        }
+        return num;
     }
+    //双指针法
+    public int removeDuplicates1(int[] nums){
+        if (nums.length==0) return 0;
+        int i=0;
+        for (int j=i+1;j<nums.length;j++){
+            if (nums[i]!=nums[j]){
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+        return i+1;
+
+    }
+
+    /**
+     * 27. 移除元素
+     *
+     * 给定一个数组 nums 和一个值 val，你需要原地移除所有数值等于 val 的元素，返回移除后数组的新长度。
+     * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+     * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+     *
+     * 示例 :
+     * 给定 nums = [0,1,2,2,3,0,4,2], val = 2,
+     * 函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4。
+     * 注意这五个元素可为任意顺序。
+     * 你不需要考虑数组中超出新长度后面的元素。
+     *
+     * @param nums
+     * @param val
+     * @return
+     */
+    public int removeElement(int[] nums, int val) {
+        if (nums.length==0) return 0;
+        int i=0;
+        for (int j=i;j<nums.length;j++){
+            if (nums[j]!=val){
+                nums[i]=nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+    public int removeElement1(int[] nums, int val){
+        int n=nums.length;
+        int i=0;
+        while (i<n){
+            if (nums[i]==val){
+                nums[i]=nums[n-1];
+                n--;
+            }else {
+                i++;
+            }
+        }
+        return n;
+    }
+
 }
