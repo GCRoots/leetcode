f83f968ceead5a18fdd85bdec92b9755da9f0efc
GC <1007879951@qq.com>
2020-02-28 23:39:44 +0800

    easy 155

--- src/main/java/easy/Solution.java
+++ src/main/java/easy/Solution.java
@@ -1560,6 +1560,72 @@ public class Solution {
     }
 
 
+    /**
+     * 155. 最小栈
+     *
+     * 设计一个支持 push，pop，top 操作，并能在常数时间内检索到最小元素的栈。
+     *
+     * push(x) -- 将元素 x 推入栈中。
+     * pop() -- 删除栈顶的元素。
+     * top() -- 获取栈顶元素。
+     * getMin() -- 检索栈中的最小元素。
+     * 示例:
+     *
+     * MinStack minStack = new MinStack();
+     * minStack.push(-2);
+     * minStack.push(0);
+     * minStack.push(-3);
+     * minStack.getMin();   --> 返回 -3.
+     * minStack.pop();
+     * minStack.top();      --> 返回 0.
+     * minStack.getMin();   --> 返回 -2.
+     *
+     */
+    class MinStack {
+        private Stack<Integer> stack;
+        private Stack<Integer> min;
+
+
+        /** initialize your data structure here. */
+        public MinStack() {
+            this.stack=new Stack<>();
+            this.min=new Stack<>();
+        }
+
+        public void push(int x) {
+            stack.push(x);
+            if (min.isEmpty()||min.peek()>=x){
+                min.push(x);
+            }
+        }
+
+        public void pop() {
+            int top=stack.pop();
+            if (min.peek().equals(top))
+                min.pop();
+        }
+
+        public int top() {
+            if (!stack.isEmpty())
+                return stack.peek();
+            else throw new RuntimeException("NO ELEMENT!!!");
+        }
+
+        public int getMin() {
+            if (!min.isEmpty())
+                return min.peek();
+            else throw new RuntimeException("NO ELEMENT!!!");
+        }
+    }
+
+
+
+
+
+
+
+
+
 
 
 
